1d369f3393601fb020ccb18b66ae85ecdc79e85a
Steve Baird <stevejbaird@gmail.com>
2016-12-12 19:08:13 +0000

    lein-release plugin: preparing 1.0.0 release

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject lein-set-git-info "1.0.0-SNAPSHOT"
+(defproject lein-set-git-info "1.0.0"
   :description "A Leiningen plugin to inject git commit information into project files."
   :url "https://github.com/displacement-activity/lein-set-git-info"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
-  :dependencies [[clj-jgit "0.8.9"]])
+  :dependencies [[clj-jgit "0.8.9"]]
+  :lein-release {:deploy-via :clojars})
